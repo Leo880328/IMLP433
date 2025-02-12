83b556d5913806a27a46d2c73e1e25eeb4f83d71
Leo <ch570991400@gmail.com>
2025-02-12 22:58:19 +0800

    Finish HW_Unit02, Unit03

---
+++ .ipynb_checkpoints/p01-checkpoint.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 21 20:06:02 2025
+
+@author: User
+"""
+
+import Mytime
+
+t1 = 777
+
+print(Mytime.GetTime("%Y-%m-%d %H:%M:%S"))
+print(Mytime.t1)
+print(t1)
+print(Mytime.time.time())
